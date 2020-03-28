27866066666a56c78e109529daed85984247a2f9
dalyIsaac <isaac.daly@outlook.com>
2020-03-28 20:44:11 +1300

    Added .NET scaffold.

---
+++ monitor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace monitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
