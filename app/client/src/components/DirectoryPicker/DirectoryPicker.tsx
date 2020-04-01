265fe0eaa47eebe01fa8a116aa8fc7872523724d
dalyIsaac <isaac.daly@outlook.com>
2020-04-01 21:25:16 +1300

    Added useOnUpdatedPath tests. Extracted the updated path useEffect into it's own hook for testing.

--- app/client/src/components/DirectoryPicker/DirectoryPicker.tsx
+++ app/client/src/components/DirectoryPicker/DirectoryPicker.tsx
@@ -1,25 +1,19 @@
 import React, { useCallback, useEffect, useRef } from "react";
-import {
-  SelectedPath,
-  addRoot,
-  setIsExpanded,
-  setOS,
-  setPath,
-  toggle,
-} from "./state";
-import { addSeparator, getParent } from "./directory";
+import { SelectedPath, addRoot, setOS, setPath, toggle } from "./state";
 import { useDispatch, useSelector } from "react-redux";
 
 import FileSystem from "../FileSystem";
 import { State } from "../../store";
 import api from "@index/api/dirs";
 import useGetChildrenAndParents from "./useGetChildrenAndParents";
+import useOnUpdatedPath from "./useOnUpdatedPath";
 
 const DirectoryPicker = (): JSX.Element => {
   const state = useSelector((state: State) => state.directoryPicker);
   const dispatch = useDispatch();
 
   const getChildrenAndParents = useGetChildrenAndParents();
+  const onUpdatedPath = useOnUpdatedPath();
 
   const alreadyRun = useRef(false);
   useEffect(() => {
@@ -39,52 +33,15 @@ const DirectoryPicker = (): JSX.Element => {
     }
   }, [dispatch, getChildrenAndParents, state.fileSystem.separator]);
 
-  // Used in the next useEffect.
-  const previousNewPath = useRef("");
-
   useEffect(() => {
-    // Runs for each update to the path.
-    const newPath = addSeparator(state.path, state.fileSystem.separator);
-    // Prevents unnecessary requests, as useEffect will be triggered by various
-    // updates to the state, which aren't path updates.
-    if (newPath !== previousNewPath.current) {
-      previousNewPath.current = newPath;
-
-      const node = state.fileSystem.items[newPath];
-      // Expands if input from keyboard, as mouse input doesn't have a separator
-      // at the end.
-      if (node && newPath === state.path) {
-        dispatch(setIsExpanded(newPath));
-      }
-
-      if (node && node.children === undefined) {
-        getChildrenAndParents(newPath);
-      } else if (!node) {
-        const parent = getParent(newPath, state.fileSystem.separator);
-        const parentNode = state.fileSystem.items[parent];
-
-        if (parentNode && !parentNode.isExpanded) {
-          dispatch(setIsExpanded(parent, true));
-          if (!parentNode.children) {
-            getChildrenAndParents(newPath);
-          }
-        }
-      }
-    }
-  }, [
-    state.differentParent,
-    state.path,
-    state.fileSystem.separator,
-    state.fileSystem.items,
-    getChildrenAndParents,
-    dispatch,
-  ]);
+    onUpdatedPath();
+  }, [onUpdatedPath]);
 
   useEffect(() => {}, []);
 
   const onChangeText = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
-      dispatch(setPath(e.currentTarget.value));
+      dispatch(setPath(e.target.value));
     },
     [dispatch],
   );
